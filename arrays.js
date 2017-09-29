bfd0e1140076b06d53e9c6525a9d0c7915036930
skalum <stkalum@gmail.com>
2017-09-29 17:59:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = [...array, element];
+  array.push(element);
   return array;
 }
